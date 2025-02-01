27595ec795fa42b50cb0982fbd1a0dfed19aeb09
ethan <ethan.martin@spacetalk.co>
2025-02-01 16:37:14 +1030

    starting to to commit font changes
    need to get new york working, even just to know that I can change the font

--- lusta-slms-next-app/tailwind.config.ts
+++ lusta-slms-next-app/tailwind.config.ts
@@ -11,13 +11,26 @@ export default {
   	extend: {
   		colors: {
   			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+  			foreground: 'var(--foreground)',
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontFamily: {
+			serif: ["var(--font-newyork)", ...fontFamily.serif],
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
